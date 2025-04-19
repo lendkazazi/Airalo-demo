2a54ac451d192d6735c463f02f727196c01432b4
Lend Kazazi <lend.kazazi1998@gmail.com>
2025-04-19 09:23:36 +0200

    - Initial commit

---
+++ Airalo Demo/Airalo Demo/Airalo_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airalo_DemoApp.swift
+//  Airalo Demo
+//
+//  Created by Baboon on 19.4.25.
+//
+
+import SwiftUI
+
+@main
+struct Airalo_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airalo Demo/Airalo DemoTests/Airalo_DemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  Airalo_DemoTests.swift
+//  Airalo DemoTests
+//
+//  Created by Baboon on 19.4.25.
+//
+
+import Testing
+
+struct Airalo_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
