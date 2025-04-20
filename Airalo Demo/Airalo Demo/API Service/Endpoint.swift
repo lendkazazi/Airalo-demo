6633c5f50ba997bdf74e177930cae9299a03628b
Lend Kazazi <lend.kazazi1998@gmail.com>
2025-04-20 10:20:13 +0200

    - API-integration - Added new API Service singleton class for handling all API calls - Added new enum for generating API endpoints - Added base url struct for better readability

---
+++ Airalo Demo/Airalo Demo/API Service/Endpoint.swift
@@ -0,0 +1,26 @@
+//
+//  Endpoint.swift
+//  Airalo Demo
+//
+//  Created by Baboon on 20.4.25.
+//
+
+import Foundation
+
+enum Endpoint {
+    case countries
+    case packages(countrySlug: String)
+
+    var path: String {
+        switch self {
+        case .countries:
+            return "/countries?type=popular"
+        case .packages(let id):
+            return "/countries/\(id)"
+        }
+    }
+
+    var url: URL {
+        return URL(string: API.baseURL + path)!
+    }
+}
